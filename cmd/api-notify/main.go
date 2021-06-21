0ce735a277d9df78bfc1e267350e6c62e904f793
Mikael Nordberg <mikael.nordberg@masarinconsulting.se>
2021-06-21 20:58:36 +0200

    flyttat om lite

--- cmd/api-notify/main.go
+++ cmd/api-notify/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 
 	app "github.com/diwise/api-notify/internal/pkg/application"
 	repo "github.com/diwise/api-notify/internal/pkg/database"
+
+	mq "github.com/diwise/messaging-golang/pkg/messaging"
 )
 
 func main() {
 	r := chi.NewRouter()
 	database := repo.NewDatabase(os.Getenv("DATABASE_URL"))
-	a := app.NewApplication(r, *database)
-	a.Start(os.Getenv("SERVICE_PORT"))
+
+	ctx, _ := mq.Initialize(mq.Config{
+		Host:        os.Getenv("RABBITMQ_HOST"),
+		User:        os.Getenv("RABBITMQ_USER"),
+		Password:    os.Getenv("RABBITMQ_PASSWORD"),
+		ServiceName: "api-notify",
+	})
+
+	defer ctx.Close()
+
+	a := app.NewApplication(r, database, ctx)
+	log.Fatal(a.Start(os.Getenv("SERVICE_PORT")))
 }
