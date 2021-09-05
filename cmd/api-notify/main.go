7edff024c417c99ff63189a6564820c74a01a519
Isak Styf <isak.styf@masarinconsulting.se>
2021-09-05 14:55:28 +0200

    Use corrent env var for reading db password

--- cmd/api-notify/main.go
+++ cmd/api-notify/main.go
@@ -18,10 +18,11 @@ func main() {
 
 	dbConfig := fmt.Sprintf(
 		"postgres://%s:%s@%s:5432/%s?sslmode=%s&pool_max_conns=10",
-		os.Getenv("DIWISE_SQLDB_USER"), os.Getenv("DIWISE_SQLDB_PASS"),
+		os.Getenv("DIWISE_SQLDB_USER"), os.Getenv("DIWISE_SQLDB_PASSWORD"),
 		os.Getenv("DIWISE_SQLDB_HOST"),
 		os.Getenv("DIWISE_SQLDB_NAME"),
 		os.Getenv("DIWISE_SQLDB_SSLMODE"))
+
 	database, err := repo.NewDatabase(dbConfig)
 
 	if err != nil {
